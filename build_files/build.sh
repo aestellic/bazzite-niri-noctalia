5417a6b013b102205d14fd4769b2aa324a9708f4
aestellic <aestellic@protonmail.com>
2026-01-31 19:35:59 +0000

    Install sane default apps

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,9 +9,27 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y remove plasma-workspace plasma-* kde-*
 
+dnf5 config-manager addrepo --from-repofile=https://repo.librewolf.net/librewolf.repo
+dnf5 config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+
+dnf5 -y install 		\
+	niri			\
+	ghostty			\
+	xdg-desktop-portal-gtk	\
+	gnome-keyring		\
+	dolphin			\
+	xwayland-satellite	\
+	noctalia-shell		\
+	librewolf		\
+	ddcutil			\
+	ark			\
+	mako			\
+	unzip			\
+	unrar			\
+	mpv			\
+	
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
